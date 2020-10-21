8640563f840abb4cdbba1640adb21c33c44f9b7c
Matt Rosanio <matt.rosanio@gmail.com>
2020-10-21 11:40:38 -0700

    Fix golf club movement

--- Assets/GolfClub.cs
+++ Assets/GolfClub.cs
@@ -1,31 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class GolfClub : MonoBehaviour
 {
-    private float yPosition;
-
-    GameObject golfBall;
+    private GameObject golfBall;
+    private float yOffsetFromGround;
 
     void Start()
     {
-        yPosition = transform.position.y;
         golfBall = GameObject.Find("GolfBall");
+        yOffsetFromGround = transform.position.y - golfBall.transform.position.y;
     }
 
     void Update()
     {
-        Plane plane = new Plane(Vector3.up, -yPosition);
+        Vector3 golfBallPosition = golfBall.transform.position;
+        Plane plane = new Plane(Vector3.up, -golfBallPosition.y);
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (plane.Raycast(ray, out float distance))
         {
             Vector3 mouseInWorld = ray.GetPoint(distance);
-            Vector3 golfBallPosition = golfBall.transform.position;
             Vector3 clubToBallVector = mouseInWorld - golfBallPosition;
             clubToBallVector.Normalize();
 
-            transform.position = golfBallPosition + clubToBallVector;
+            transform.position = golfBallPosition + clubToBallVector + new Vector3(0, yOffsetFromGround, 0);
         }
     }
 }
