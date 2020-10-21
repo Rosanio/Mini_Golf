7d3f05c84830d7db87da9f0ceb0d887ee1617dff
Matt Rosanio <matt.rosanio@gmail.com>
2020-10-20 20:42:30 -0700

    Initial commit

---
+++ Assets/GolfClub.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfClub : MonoBehaviour
+{
+    private float yPosition;
+
+    GameObject golfBall;
+
+    void Start()
+    {
+        yPosition = transform.position.y;
+        golfBall = GameObject.Find("GolfBall");
+    }
+
+    void Update()
+    {
+        Plane plane = new Plane(Vector3.up, -yPosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (plane.Raycast(ray, out float distance))
+        {
+            Vector3 mouseInWorld = ray.GetPoint(distance);
+            Vector3 golfBallPosition = golfBall.transform.position;
+            Vector3 clubToBallVector = mouseInWorld - golfBallPosition;
+            clubToBallVector.Normalize();
+
+            transform.position = golfBallPosition + clubToBallVector;
+        }
+    }
+}
