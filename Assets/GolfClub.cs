cf8fe0bea42f5056a9574d35ed366e5b8e28b2e2
Matt Rosanio <matt.rosanio@gmail.com>
2020-12-17 11:03:30 -0500

    Create level 5

--- Assets/GolfClub.cs
+++ Assets/GolfClub.cs
@@ -289,6 +289,7 @@ public class GolfClub : MonoBehaviour
             case 1: return 5;
             case 2: return 2;
             case 3: return 2;
+            case 4: return 4;
             default: return 0;
         }
     }
